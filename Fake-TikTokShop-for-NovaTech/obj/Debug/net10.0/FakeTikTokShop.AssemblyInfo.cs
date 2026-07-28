d72a14ea2e856e1396d547ff3961638bf16932dc
Hoshi2107 <hoshi.kagayaku2008@gmail.com>
2026-07-28 19:33:56 +0700

    Change text color for more clearly to see

--- Fake-TikTokShop-for-NovaTech/obj/Debug/net10.0/FakeTikTokShop.AssemblyInfo.cs
+++ Fake-TikTokShop-for-NovaTech/obj/Debug/net10.0/FakeTikTokShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FakeTikTokShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aef986ae6579129d3dae31c79fd288b932b0202")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da49d07d59a5a8448879e0049a50048b3075eab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FakeTikTokShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FakeTikTokShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
